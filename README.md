69f5e82aece28e959e36433c2c379afe2f0dc9c5
Samuli Tani <samuli.tani@gmail.com>
2026-02-03 15:06:32 +0200

    feat: started with calls to ollama and FastAPI

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ An app for searching conract information from websites
 
 The progamm uses LLM to find contact information from list of websites. For example you can provide list of schools and programm tries to find principals email, phone number etc.
 
-
 ## Authors
 
 - [@Prshkv](https://www.github.com/Prshkv)
@@ -14,12 +13,14 @@ The progamm uses LLM to find contact information from list of websites. For exam
 - [@Matimane](https://www.github.com/Matimane)
 - [@Eetuhellberg](https://www.github.com/eetuhellberg)
 
-
-
-
 ## Tech Stack
 
-**Client:** JavaScript/Typescript React 
+**Client:** JavaScript/Typescript React
 
 **Server:** Python, FastAPI
 
+## Backend
+
+### External documentation
+
+[Langchain Ollama documentation](https://reference.langchain.com/python/integrations/langchain_ollama/?_gl=1*1ggao8h*_gcl_au*ODU1NTg1NTc2LjE3Njk3NjE0NDQ.*_ga*Mzg5Mzc0NTU4LjE3Njk3NjE0NDQ.*_ga_47WX3HKKY2*czE3NzAxMTQyOTUkbzIkZzEkdDE3NzAxMTQ0NzEkajUkbDAkaDA.#langchain_ollama.ChatOllama.base_url)
--- samples/ollamatest.py
+++ samples/ollamatest.py
@@ -11,6 +11,25 @@ OLLAMA_USERNAME = os.getenv("OLLAMA_USERNAME")
 OLLAMA_PASSWORD = os.getenv("OLLAMA_PASSWORD")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 
+model_list = [
+    "gemma3:12b",
+    "llama3.3:70b",
+    "qwen3-coder:latest",
+    "gpt-oss:120b",
+    "gpt-oss:20b",
+    "qwen3-next:latest",
+    "ministral-3:8b",
+    "translategemma:4b",
+    "translategemma:12b",
+    "translategemma:27b",
+]
+
+for i, model in enumerate(model_list):
+    print(f"{i} {model}")
+
+OLLAMA_MODEL = model_list[int(input("Choose a model: "))]
+print(f"Chosen model: {OLLAMA_MODEL}")
+
 urllib3.disable_warnings(
     urllib3.exceptions.InsecureRequestWarning
 )  # this is because of server's self signed certificate
@@ -28,7 +47,7 @@ data = {
     "options": {
         "temperature": 0.0,  # 0.0 is most determinate
         "top_p": 1.0,  # 1.0 is most determinate
-        "num_predict": 500,  # limit the response to n tokens
+        "num_predict": 1500,  # limit the response to n tokens
     },
 }
 
