4e7a14a1245c41966160c787900e2102bd53129c
Samuli Tani <samuli.tani@tietopolku.fi>
2026-02-14 16:54:11 +0200

    feat(prompts): add contact extraction prompt logic

---
+++ src/prompts/__init__.py
@@ -0,0 +1,4 @@
+print("prompts/contact_extraction loading")
+
+from .contact_extraction import SystemMessage
+from .contact_extraction import UserPrompt
---
+++ src/prompts/contact_extraction.py
@@ -0,0 +1,34 @@
+class SystemMessage:
+    def __init__(self, occupations: list[str] | None) -> None:
+        self.occupations = ", ".join(occupations) if occupations else None
+        self.personality = "You are a data extraction assistant specialized in extracting contact information from webpages."
+        if self.occupations:
+            self.task = f"TASK: Extract contact details of following occupations/roles found in the provided webpage content: ({self.occupations})"
+        else:
+            self.task = f"TASK: Extract all contact details found in the provided webpage content."
+        self.output_format = "OUTPUT FORMAT: Return a valid JSON list of objects."
+        self.rules = "RULES:- Construct the email address if one is explicitly provided or implicitly given as a rule of forming it from contact details."
+
+    def create(self):
+        sys_message = self.personality + "\n" + self.task + "\n" + self.rules
+        return sys_message
+
+
+class UserPrompt:
+    def __init__(self, web_content: str):
+        self.prompt = f"Here is the content of a webpage:\n\n{web_content}"
+
+    def create(self):
+        return self.prompt
+
+
+def main():
+    sm = SystemMessage(["Pappi", "Lukkari", "Talonpoika", "Varas"])
+    up = UserPrompt("<html>Kotisivu</html>")
+
+    print(sm.create())
+    print(up.create())
+
+
+if __name__ == "__main__":
+    main()
