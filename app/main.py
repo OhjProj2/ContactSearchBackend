d4476aa9b13a8f244d998c2225b24bb22a402de1
Samuli Tani <samuli.tani@myy.haaga-helia.fi>
2026-02-17 22:47:20 +0200

    feat: CORS settings for development

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
-
-from routers.seek import router as seek_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from routers.seek import router as seek_router
 app.include_router(seek_router, tags=["Contact Extraction"])
 
