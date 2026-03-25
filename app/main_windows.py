6212fe6aea3c872ce8a448e066259681523c2908
eetuh098@outlook.com <eetuh098@outlook.com>
2026-03-25 13:06:07 +0200

    crawl4ai error handling and main_windows update. seek, crawler and main_windows files changed

--- app/main_windows.py
+++ app/main_windows.py
@@ -9,6 +9,12 @@ from fastapi.middleware.cors import CORSMiddleware
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
+from app.routers.seek import router as seek_router
+from app.routers.listmodels import router as listmodels_router
+from app.routers.listdbs import router as listdbs_router
+from app.routers.listcollections import router as listcollections_router
+from app.routers.listalldata import router as listalldata_router
+
 app = FastAPI()
 
 app.add_middleware(
@@ -19,8 +25,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from routers.seek import router as seek_router
 app.include_router(seek_router, tags=["Contact Extraction"])
+app.include_router(listmodels_router, tags=["List Models"])
+app.include_router(listdbs_router, tags=["List Databases"])
+app.include_router(listcollections_router, tags=["List Collections"])
+app.include_router(listalldata_router, tags=["List Saved Data"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app.main_windows:app", host="127.0.0.1", port=8000, reload=False)
