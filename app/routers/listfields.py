a47dbd6d291530a2aa1117f7ead86066870b2d1c
Matimane <1matias.sainio@gmail.com>
2026-04-06 16:36:15 +0300

    added var_name to listfields

--- app/routers/listfields.py
+++ app/routers/listfields.py
@@ -7,5 +7,4 @@ router = APIRouter()
 @router.get("/listfields")
 async def list_fields():
     """Router that gets list of all default fields."""
-    default_field_names = [ContactField.name for ContactField in default_fields]
-    return default_field_names
+    return [{"label": field.name, "value": field.var_name} for field in default_fields]
